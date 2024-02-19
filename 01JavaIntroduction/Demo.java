12cb3e7983d5d8eac9ffaf034cad6275f887a388
aniket07ambat <ambataniket07@gmail.com>
2024-02-19 14:04:01 +0530

    Started Learning Java

---
+++ 01JavaIntroduction/Demo.java
@@ -0,0 +1,25 @@
+// Notes
+// javac FileName.java : converts our code into byte code i.e is .class file.
+// So Now File.class is generated.
+// java FileName: to run our code.
+
+// public: Public means this class is accessed by anyone.
+
+// class : class is just named group of properties and functions
+// methods : All the functions in the class are called methods.
+
+// Demo : Demo function is entry point for Java program.
+// If you write your file name "Demo.java" then you must write 
+// "public class Demo" you can't write "public class anyOtherName." 
+
+// static : with this we can excute our code without needing to initiate a class making it the entry point of the program.
+
+
+
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
